7f49503c8840903a4136f8563aee50f7767a461e
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-09-26 03:35:37 +0700

    added basic input field, button, and input handle

--- 01-android-starter/01-test/app/src/main/java/com/dicoding/myapplication/MainActivity.kt
+++ 01-android-starter/01-test/app/src/main/java/com/dicoding/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.dicoding.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //ctrl alt L -> auto format
+
+        var input_field = findViewById<EditText>(R.id.in_tgl_lahir)
+        var btnKlik = findViewById<Button>(R.id.button_submit)
+        var generasi = findViewById<TextView>(R.id.generasi)
+
+        btnKlik.setOnClickListener {
+            val input = input_field.text.toString()
+            val hasil = when (input.toInt()){
+                in 1946..1964 -> "Baby Boomers"
+                in 1965..1980 -> "X"
+                in 1981..1995 -> "Millenial"
+                in 1996..2010 -> "Z"
+
+                else -> "gatau lu apa man"
+            }
+            generasi.text = "You are in $hasil generations"
         }
     }
 }
