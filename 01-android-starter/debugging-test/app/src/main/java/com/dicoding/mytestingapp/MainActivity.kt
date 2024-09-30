faef085cffb1485994b4b9e2f6399f89b52ca93e
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-09-30 18:02:21 +0700

    learning debugging

--- 01-android-starter/debugging-test/app/src/main/java/com/dicoding/mytestingapp/MainActivity.kt
+++ 01-android-starter/debugging-test/app/src/main/java/com/dicoding/mytestingapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.dicoding.mytestingapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    //private var btnSetValue: Button? = null //-> penyebab error, null
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +26,37 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue = findViewById(R.id.btn_set_value)
+
+        //sebelumnya blm di inisialisasi, mknya error
+        btnSetValue!!.setOnClickListener(this)
+
+        names.add("Narenda Wicaksono")
+        names.add("Kevin")
+        names.add("Yoza")
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_set_value) {
+            tvText.text = "19"
+
+            /*
+            Log.e() untuk log error.
+            Log.w() untuk log warning.
+            Log.i() untuk log information.
+            Log.d() untuk log debug.
+            Log.v() untuk log verbose.
+             */
+
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..2) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
         }
     }
 }
