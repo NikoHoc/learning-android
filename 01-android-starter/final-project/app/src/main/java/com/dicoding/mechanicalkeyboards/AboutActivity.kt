211c75c38c5cb5124c8a37aa6a58ec054986aaa6
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-10-02 23:44:10 +0700

    added few style

--- 01-android-starter/final-project/app/src/main/java/com/dicoding/mechanicalkeyboards/AboutActivity.kt
+++ 01-android-starter/final-project/app/src/main/java/com/dicoding/mechanicalkeyboards/AboutActivity.kt
@@ -1,12 +1,16 @@
 package com.dicoding.mechanicalkeyboards
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class AboutActivity : AppCompatActivity() {
+    private lateinit var profilePicture : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +20,8 @@ class AboutActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
 
 
     }
