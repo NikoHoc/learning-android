17e6905fe0f901eba1c0e04fcc10e09a167e4140
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-10-03 17:11:04 +0700

    finish styling

--- 01-android-starter/final-project/app/build.gradle.kts
+++ 01-android-starter/final-project/app/build.gradle.kts
@@ -67,14 +67,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.android.material:material:1.12.0")
-
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation(libs.androidx.core.splashscreen)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    implementation(libs.kotlinx.coroutines.core)
+
 
 }
