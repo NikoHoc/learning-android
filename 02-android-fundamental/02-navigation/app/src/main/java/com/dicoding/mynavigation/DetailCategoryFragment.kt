6663bcdc07727dc312b1247304ab393946c3fc55
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-10-07 02:51:38 +0700

    send data with bundle

--- 02-android-fundamental/02-navigation/app/src/main/java/com/dicoding/mynavigation/DetailCategoryFragment.kt
+++ 02-android-fundamental/02-navigation/app/src/main/java/com/dicoding/mynavigation/DetailCategoryFragment.kt
@@ -5,18 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import com.dicoding.mynavigation.databinding.FragmentHomeBinding
+import com.dicoding.mynavigation.databinding.FragmentDetailCategoryBinding
 
-class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
+class DetailCategoryFragment : Fragment() {
+    private var _binding: FragmentDetailCategoryBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
@@ -24,19 +22,15 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnCategory.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment)
-        )
-        binding.btnProfile.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_homeFragment_to_profileActivity)
-        }
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
-
-
-
 }
