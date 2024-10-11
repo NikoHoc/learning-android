b758caf67a4a63325dbe5ed79fed0a9fa5ec3480
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-10-12 01:58:00 +0700

    retrofit for getting API

--- 02-android-fundamental/RestaurantReview/app/build.gradle.kts
+++ 02-android-fundamental/RestaurantReview/app/build.gradle.kts
@@ -26,6 +26,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -45,4 +50,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.glide)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
 }
