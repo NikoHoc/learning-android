2b4c74095d5516af38437afe21b8d06015d0e9c4
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-10-11 19:58:37 +0700

    trying handler & executor, and trying using coroutines

--- 02-android-fundamental/07-asynchronous-background-thread/app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
+++ 02-android-fundamental/07-asynchronous-background-thread/app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -1,10 +1,20 @@
 package com.dicoding.mybackgroundthread
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +26,64 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        // kode dibawah jika menggunakan executor dan handler biasa
+//        btnStart.setOnClickListener {
+//            // tambahkan executor disini sebelum try
+//            executor.execute {
+//                try {
+//                    //simulate process compressing
+//                    for (i in 0..10) {
+//                        Thread.sleep(500)
+//                        val percentage = i * 10
+//                        // sebelumnya berjalan di UI thread -> bikin lag/freeze
+////                    if (percentage == 100) {
+////                        tvStatus.setText(R.string.task_completed)
+////                    } else {
+////                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+////                    }
+//
+//                        // setelah ditambahkan handler
+//                        handler.post {
+//                            //update ui in main thread
+//                            if (percentage == 100) {
+//                                tvStatus.setText(R.string.task_completed)
+//                            } else {
+//                                tvStatus.text =
+//                                    String.format(getString(R.string.compressing), percentage)
+//                            }
+//                        }
+//                    }
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//        }
+
+        // begini jika menggunakan coroutines, tidak perlu executor dan handler
+        btnStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default) {
+                //simulate process in background thread
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        //update ui in main thread
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
