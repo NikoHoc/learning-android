eef49f5fe515f443dc6080b1a59491d5a9846545
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-10-13 03:03:42 +0700

    add event response, and APIconfig, APIservice

---
+++ 02-android-fundamental/dicoding-event/app/src/main/java/com/dicoding/dicodingevent/data/retrofit/ApiService.kt
@@ -0,0 +1,23 @@
+package com.dicoding.dicodingevent.data.retrofit
+
+import com.dicoding.dicodingevent.data.response.EventResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    // untuk list event dan optional jika search
+    // active -> 1: upcoming, 0: finished, -1: all events
+    @GET("/events")
+    fun getListEvents(
+        @Query("active") active: Int,
+        @Query("q") query: String? = null
+    ): Call<EventResponse>
+
+    // untuk detail event
+    @GET("/events/{id}")
+    fun getDetailEvent(
+        @Path("id") id: String
+    ): Call<EventResponse>
+
+
+}
--- 02-android-fundamental/dicoding-event/app/src/main/java/com/dicoding/dicodingevent/ui/MainActivity.kt
+++ 02-android-fundamental/dicoding-event/app/src/main/java/com/dicoding/dicodingevent/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dicoding.dicodingevent
+package com.dicoding.dicodingevent.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.dicoding.dicodingevent.R
 import com.dicoding.dicodingevent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
